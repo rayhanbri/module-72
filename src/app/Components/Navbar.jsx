0257a47197d6fa4b3f06397d3540b749cbba9f91
rayhan <mohammadrayhanx0@gmail.com>
2025-07-27 22:46:47 +0600

    navigation added

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,12 +1,14 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     return (
         <nav className='flex justify-center my-3'>
             <ul className='flex justify-around w-1/2'>
-                <li>Home</li>
-                <li>Services</li>
-                <li>About</li>
+                <Link href='/'><li>Home</li></Link>
+                <Link href='/services'><li>Services</li></Link>
+                <Link href='/about'><li>About</li></Link>
+                
             </ul>
         </nav>
     );
--- src/app/about/address/page.jsx
+++ src/app/about/address/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const AdressPage = () => {
+    // forder er modde page.jsx thaklei kebol tha route bole bibechito hobe naile hobe na 
     return (
         <div>
             <h1 className='text-4xl font-bold text-amber-200'>Address Page</h1>
