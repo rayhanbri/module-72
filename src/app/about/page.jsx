b3d4c1fd40610a278a0ac34e5a526f77ab71820d
rayhan <mohammadrayhanx0@gmail.com>
2025-07-27 22:59:22 +0600

    use router added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,25 @@
+'use client'
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
+    const router = useRouter()
+    const isLoggedin = false;
+    const handleNavigation=() =>{
+        if(isLoggedin){
+            router.push('/about/address')
+        }
+        else{
+            router.push('/')
+        }
+        
+    }
     return (
         <div className='font-bold'>
             i am about
+             <Link href='/about/address'><li>Address Page</li></Link>
+             <button onClick={handleNavigation} className='button text-2xl text-sky-300'>Address Page</button>
         </div>
     );
 };
