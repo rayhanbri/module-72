3f5863694daac7f3d239512389b15ca2cf54ac94
rayhan <mohammadrayhanx0@gmail.com>
2025-07-28 01:07:21 +0600

    catch all

---
+++ src/app/about/[...slugs]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutSlugPages = async({params}) => {
+    const p = await params;
+    console.log(p)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AboutSlugPages;
