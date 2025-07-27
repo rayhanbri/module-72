93d66b91bedbaaf61c3a9a2d49936a87ff05af5f
rayhan <mohammadrayhanx0@gmail.com>
2025-07-27 22:33:49 +0600

    nested layout in about

---
+++ src/app/about/address/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdressPage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold text-amber-200'>Address Page</h1>
+        </div>
+    );
+};
+
+export default AdressPage;
