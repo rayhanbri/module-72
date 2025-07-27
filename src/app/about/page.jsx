ca914ab5821af9755e885e55ebf78758fb0ba1f1
rayhan <mohammadrayhanx0@gmail.com>
2025-07-27 22:30:05 +0600

    services and about route

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='font-bold'>
+            i am about
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='font-bold text-sky-400 text-5xl'>
+            i am servicespage
+        </div>
+    );
+};
+
+export default page;
