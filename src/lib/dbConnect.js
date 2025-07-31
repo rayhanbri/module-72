743f072e0fefa26e0094f4fbabf2d759828b5ceb
rayhan <mohammadrayhanx0@gmail.com>
2025-08-01 02:58:50 +0600

    mongo db connected

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,16 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+// ekhane const kaj kore na ei jonno import 
+
+function dbConnect(collectionName) {
+    const uri = process.env.MONGO_URI;
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    return client.db(process.env.USER_NAME).collection(collectionName)
+}
+
