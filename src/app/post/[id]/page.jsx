a07988a3b95b39b66b7a177da87196938f7dfb10
rayhan <mohammadrayhanx0@gmail.com>
2025-07-31 22:59:13 +0600

    meta data page.details.jsx

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -6,6 +6,12 @@ const getSinglePost = async (id) => {
     return data;
 }
 
+export const metadata = {
+  title: "All Post | Learning Next.js",
+  description: "getting all post by fetching",
+};
+
+
 const SinglePost = async ({ params }) => {
     const p = await params;
     const id = p.id;
