bf652606bf478a5a518abadedc45d673e93065ce
rayhan <mohammadrayhanx0@gmail.com>
2025-08-01 02:37:11 +0600

    delete and patach function

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -5,3 +5,19 @@ export async function GET(req,params) {
  
     return Response.json({p})
 }
+
+
+export async function DELETE(req,params) {
+
+    const p =await  params; 
+    console.log(p)
+ 
+    return Response.json({p})
+}
+export async function PATCH(req,params) {
+
+    const p =await  params; 
+    console.log(p)
+ 
+    return Response.json({p})
+}
