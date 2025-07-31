47ad400ba45e8f1194bf3d8bfad3b1a76d4e2452
rayhan <mohammadrayhanx0@gmail.com>
2025-08-01 02:26:07 +0600

    post route added

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -1,9 +1,15 @@
 export async function GET() {
     // bowser api/items marle eita dhekacce 
-  const data = {
-    message:"api is succesfull",
-    error:false,
-    status:200
-  } 
-  return Response.json({ data })
+    const data = {
+        message: "api is succesfull",
+        error: false,
+        status: 200
+    }
+    return Response.json({ data })
+}
+
+export async function POST(req) {
+    // bowser api/items marle eita dhekacce 
+    const postedData =await req.json();
+    return Response.json({ postedData })
 }
