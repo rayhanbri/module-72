9305eb5a0ec1f5121c1c7c4ee13aeba1206c3744
rayhan <mohammadrayhanx0@gmail.com>
2025-08-01 03:35:32 +0600

    getting single data

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,23 +1,30 @@
-export async function GET(req,params) {
+import { dbConnect } from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
 
-    const p =await  params; 
-    console.log(p)
- 
-    return Response.json({p})
+export async function GET(req,{params}) {
+    // give your database name to the function 
+   
+    // const params = await req.params;
+
+    // console.log('hiiii',req)
+
+    const p = await params.id;
+    console.log( 'heelooo',p)
+    const singleData= await dbConnect('test_data').findOne({_id : new ObjectId(p)});
+    return Response.json({singleData})
 }
 
+export async function DELETE(req, params) {
 
-export async function DELETE(req,params) {
-
-    const p =await  params; 
+    const p = await params;
     console.log(p)
- 
-    return Response.json({p})
+
+    return Response.json({ p })
 }
-export async function PATCH(req,params) {
+export async function PATCH(req, params) {
 
-    const p =await  params; 
+    const p = await params;
     console.log(p)
- 
-    return Response.json({p})
+
+    return Response.json({ p })
 }
